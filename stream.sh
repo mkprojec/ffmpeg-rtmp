48eaa19738af14b5b7e4105d1b44e22a6596e22f
mkprojec <101967322+mkprojec@users.noreply.github.com>
2022-08-14 21:39:19 +0000

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -3,10 +3,10 @@
 VBR="1500k"
 FPS="30"
 QUAL="ultrafast"
-YOUTUBE_URL="rtmp"
-YOUTUBE_KEY="your key"
-VIDEO_SOURCE="your video (mp4)"
-AUDIO_SOURCE="your mp3"
+RTMP_URL="rtmps://live-api-s.facebook.com:443/rtmp/"
+RTMP_KEY="FB-548535630111633-0-AbxsW2huFgZWe4Tf"
+VIDEO_SOURCE="videos/Code.mp4"
+AUDIO_SOURCE="mp3/sound1.mp3"
 AUDIO_ENCODER="aac"
 
 ffmpeg \
@@ -14,8 +14,9 @@ ffmpeg \
  -re \
  -i "$VIDEO_SOURCE" \
  -thread_queue_size 512 \
+ -stream_loop -1 \
  -i "$AUDIO_SOURCE" \
  -c:v libx264 -preset $QUAL -r $FPS -g $(($FPS *2)) -b:v $VBR \
  -c:a $AUDIO_ENCODER -threads 6 -ar 44100 -b:a 128k -bufsize 512k -pix_fmt yuv420p \
  -fflags +shortest -max_interleave_delta 50000 \
- -f flv $YOUTUBE_URL/$YOUTUBE_KEY
+ -f flv $RTMP_URL/$RTMP_KEY
