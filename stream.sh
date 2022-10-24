ecbc93476c74c4d6d9c0d02b6348cf26db6e3e49
mkprojec <101967322+mkprojec@users.noreply.github.com>
2022-10-24 20:10:28 +0700

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -2,7 +2,7 @@
 
 VBR="1500k"
 FPS="30"
-QUAL="ultrafast"
+QUAL="veryfast"
 RTMP_URL="rtmps"
 RTMP_KEY="rtmp key"
 VIDEO_SOURCE="videos/your.mp4"
@@ -16,7 +16,7 @@ ffmpeg \
  -thread_queue_size 512 \
  -stream_loop -1 \
  -i "$AUDIO_SOURCE" \
- -c:v libx264 -preset $QUAL -r $FPS -g $(($FPS *2)) -b:v $VBR \
- -c:a $AUDIO_ENCODER -threads 6 -ar 44100 -b:a 128k -bufsize 512k -pix_fmt yuv420p \
+ -c:v libx264 -preset $QUAL -r $FPS -g $FPS -b:v $VBR \
+ -c:a $AUDIO_ENCODER -threads 6 -ar 44100 -b:a 128k -bufsize 512k -pix_fmt yuv420p -s 1280x720 \
  -fflags +shortest -max_interleave_delta 50000 \
  -f flv $RTMP_URL/$RTMP_KEY
